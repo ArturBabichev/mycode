d5ceb2705dff855b4601d1320b8a5ef0d0bb07d2
Artur Babichev <79402964+ArturBabichev@users.noreply.github.com>
2021-02-21 19:54:41 +0100

    Block for web site
    Header, class nav. slider, text

--- joker7.php
+++ joker7.php
@@ -1,15 +1,67 @@
 <?php
 /**
-*Template Name: joker
+*Template Name: CHIKA
 Template Post Type:post,page
 */
 ?>
-<template id="hacej">
-Template Post Type:post,page
-*/
 <!DOCTYPE HTML>
 <html lang="ru">
   <head>
     <meta charset="UTF-8">
-<link rel="stylesheet" href="http://www.hackup.top/wp-content/themes/twentytwelve/css/JORKE.CSS.css">
+<link rel="stylesheet" href="http://www.hackup.top/JORKE.CSS.css">
 <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Montserrat:ital@1&display=swap" rel="stylesheet">
+    <title>HACK</title>
+    </head>
+    <body>
+        
+        <header class="header">
+            <div class="container">
+                <div clas="header__inner">
+         <div class="header__logo">MoGO</div>
+                    
+                    
+<nav class="nav">
+ <a class="nav__link active" href='#'>About</a>
+ <a class="nav__link" href='#'>Service</a>
+ <a class="nav__link" href='#'>Blog</a>
+ <a class="nav__link" href='#'>Work</a>
+ <a class="nav__link" href='#'>Contact</a>
+</nav>
+ </div>
+</div>
+        </header>
+        
+
+        <div class="intro">
+             <div class="container">
+                <div class="intro__inner">
+                    <h2 class="intro__suptitle">Creative template</h2>
+              <h1 class="intro__title">Welcome to MoGo</h1>
+<a class="btn" href="#">Learn More</a>
+   </div>
+     </div>
+            
+    <div class="slider">
+<div class="container">
+    <div class="slider__inner">
+<div class="slider__item active">
+    <span class="slider__num">01</span>
+    Intro
+</div> 
+ <div class="slider__item">
+    <span class="slider__num">02</span>
+joker
+</div> 
+ <div class="slider__item">
+     <span class="slider__num">03</span>
+spider man
+</div> 
+ <div class="slider__item">
+     <span class="slider__num">04</span>
+batman
+</div> 
+   </div>               
+  </div>
+ </div>
+</div
