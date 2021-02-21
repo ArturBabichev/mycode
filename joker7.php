a1296a6ea9aee04c14a24e266eab9d6537209246
Artur Babichev <79402964+ArturBabichev@users.noreply.github.com>
2021-02-21 18:20:11 +0100

    My web site first block
    fonts text and navigation menu

---
+++ joker7.php
@@ -0,0 +1,15 @@
+<?php
+/**
+*Template Name: joker
+Template Post Type:post,page
+*/
+?>
+<template id="hacej">
+Template Post Type:post,page
+*/
+<!DOCTYPE HTML>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+<link rel="stylesheet" href="http://www.hackup.top/wp-content/themes/twentytwelve/css/JORKE.CSS.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
